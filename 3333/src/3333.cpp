1b91f512d5ed3aca346efd2114ff8f9dacc340e3
Kutkin-S <72380802+Kutkin-S@users.noreply.github.com>
2020-10-18 18:58:39 +0500

    Add files via upload

---
+++ 3333/src/3333.cpp
@@ -0,0 +1,74 @@
+//============================================================================
+// Name        : 3333.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <memory>
+#include <cmath>
+#include <algorithm>
+
+#include <SDL2/SDL.h>
+
+constexpr double Pi = acos(-1.);
+
+#ifdef _WIN32
+# include <SDL2/SDL_main.h>
+#endif
+
+constexpr int WIDTH = 800, HEIGHT = 600;
+double x = (Pi*2/360.0)*Pi;
+
+int main(int, char**)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	auto win = std::shared_ptr<SDL_Window>(
+			SDL_CreateWindow(
+						"Yo!",
+						SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+						WIDTH, HEIGHT,
+						SDL_WINDOW_SHOWN),
+			SDL_DestroyWindow);
+	if (win == nullptr) {
+		std::cerr << "Error creating windows: " <<
+				SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	auto ren = std::shared_ptr<SDL_Renderer>(
+			SDL_CreateRenderer(win.get(), -1,
+					SDL_RENDERER_ACCELERATED |
+					SDL_RENDERER_PRESENTVSYNC),
+			SDL_DestroyRenderer);
+	if (ren == nullptr) {
+		std::cerr << "Error creating renderer: " <<
+				SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Event event;
+
+	for (;;) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) return 0;
+		}
+
+		SDL_SetRenderDrawColor(ren.get(),48, 48, 48, 48);
+		SDL_RenderClear(ren.get());
+
+		SDL_SetRenderDrawColor(ren.get(), 0xFF, 0x00, 0xFF, 0xFF);
+
+		for(int i=-WIDTH; i<WIDTH;i++)
+				{
+			SDL_RenderDrawPoint(ren.get(), i+WIDTH/2, (30*sin(i*(Pi*2/360.0)*Pi)/(i*(Pi*2/360.0))*-Pi)+HEIGHT/2);
+				}
+
+		SDL_RenderPresent(ren.get());
+	}
+
+	return 0;
+}
